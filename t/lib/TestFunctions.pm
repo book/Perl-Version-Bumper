ad333e6549fe9049972e637cd9a69396e443f542
Philippe Bruhat (BooK) <book@cpan.org>
2024-12-23 15:15:54 +0100

    move the callback code to the test library

--- t/lib/TestFunctions.pm
+++ t/lib/TestFunctions.pm
@@ -8,6 +8,101 @@ use Perl::Version::Bumper qw(
   stable_version_inc
 );
 
+my %callback = (
+    bump => sub {
+        my ( $perv, $src, $expected, $name, $ctx ) = @_;
+        my $version = $perv->version;
+        my $this    = qq{"$name" [$version]};
+        $expected =~ s/use VERSION;/use $version;/g;
+
+        # bump_ppi
+        my $doc = $ctx->{$src} //= do {    # cache the PPI document
+            my $ppi = PPI::Document->new( \$src );
+            is( $ppi, D, "'$name' parsed by PPI" );
+            $ppi;
+        };
+        is( $perv->bump_ppi($doc)->serialize, $expected, "$this ->bump_ppi" );
+
+        # bump
+        is( $perv->bump($src), $expected, "$this ->bump" );
+
+        # bump_file
+        my $file = Path::Tiny->tempfile;
+        $file->spew($src);
+        my $ran = $perv->bump_file($file);
+        if ( $src eq $expected ) { is( $ran, !!0, "$this ->bump_file (same)" ); }
+        else                     { is( $ran, !!1, "$this ->bump_file (mod')" ); }
+        is( $file->slurp, $expected, "$this ->bump_file (expected update)" );
+    },
+    bump_safely => sub {
+        my ( $perv, $src, $expected, $name, $ctx ) = @_;
+        my $version = $perv->version;
+        my $this    = "$name [$version]";
+
+        # perform the version expectations bump
+        $expected =~ s/use VERSION;/use $version;/g;
+
+        # make a PPI document
+        my $doc = $ctx->{$src} //= do {         # cache the PPI document
+            my $ppi = PPI::Document->new( \$src );
+            is( $ppi, D, "'$name' parsed by PPI" );
+            $ppi;
+        };
+
+        # and a file
+        my $file = Path::Tiny->tempfile;
+        $file->spew($src);
+
+        my ( $ran, %got );
+        {
+            local $SIG{__WARN__} = sub { };
+
+            # silence errors
+            open( \*OLDERR, '>&', \*STDERR ) or die "Can't dup STDERR: $!";
+            open( \*STDERR, '>',  '/dev/null' )
+              or die "Can't re-open STDERR: $!";
+
+            $ran = eval { $perv->bump_file_safely($file) }
+              or my $error = $@;    # catch (syntax) errors in the eval'ed code
+
+            # get STDERR back, and warn about errors while compiling
+            open( \*STDERR, '>&', \*OLDERR ) or die "Can't restore STDERR: $!";
+
+            # throw the errors in the eval, if any
+            die $error if $error;
+
+            # collect the results
+            $got{bump_ppi_safely}  = $perv->bump_ppi_safely($doc)->serialize;
+            $got{bump_safely}      = $perv->bump_safely( $doc->serialize );
+            $got{bump_file_safely} = $file->slurp;
+        }
+
+        # test the other two subs
+        if ( $name =~ /DIE(?: *< *(v5.[0-9]+))?/ ) {    # compilation might fail
+            if ($1) {    # on an older perl binary
+                if ( $] < version_fmt($1) ) {
+                    is( $ran, U, "$this ->bump_file_safely did not compile on $^V" );
+                    $expected = $src;    # no change expected
+                }
+                else {
+                    is( $ran, D, "$this ->bump_file_safely compiled on $^V" );
+                }
+            }
+            else {    # no minimum version, always expected to fail compilation
+                is( $ran, U, "$this ->bump_file_safely did not compile on $^V" );
+                $expected = $src;    # no change expected
+            }
+        }
+        else {                       # not expected to fail compilatin
+            is( $ran, D, "$this ->bump_file_safely compiled on $^V" );
+        }
+
+        # check the expected result
+        is( $got{$_}, $expected, "$this ->$_" )
+          for qw( bump_ppi_safely bump_safely bump_file_safely );
+    },
+);
+
 sub test_dir {
     my %args = @_;
     my $dir  = path(__FILE__)->parent->parent->child( $args{dir} );
@@ -15,9 +110,12 @@ sub test_dir {
 }
 
 sub test_file {
-    my %args    = @_;
-    my $file    = $args{file};
-    my $stop_at = stable_version_inc( $args{stop_at} );
+    my %args     = @_;
+    my $file     = $args{file};
+    my $callback = $callback{ $args{callback} };
+    my $stop_at  = stable_version_inc( $args{stop_at}
+          // Perl::Version::Bumper->feature_version );
+    die "Unknown test callback '$args{callback}'" unless $callback;
 
     # blocks of test data are separated by ##########
     my @tests = split /^########## (.*)\n/m, path($file)->slurp;
@@ -59,7 +157,7 @@ sub test_file {
                 }
                 $todo &&= todo $todo;
 
-                $args{callback}->(
+                $callback->(
                     Perl::Version::Bumper->new( version => $version ),
                     $src, $expected, $name, $ctx
                 );
